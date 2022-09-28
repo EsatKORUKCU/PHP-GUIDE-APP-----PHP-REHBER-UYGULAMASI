c687df22e8cdaefade7eea75da92faee4571ad0d
Esat KÖRÜKCÜ <105414787+EsatKORUKCU@users.noreply.github.com>
2022-09-28 23:45:29 +0300

    Add files via upload

---
+++ Rehber/inc/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    // Destek: https://github.com/msbatal/PHP-PDO-Database-Class
+
+    require_once ('SunDB.php');
+    $baglan = new SunDB(null, 'localhost', 'esat', '12345', 'odev');
+    // Kullanıcı Adı, Şifre, Veritabanı Adı
+
+?>
